ed5011553ddb7a86c530d8a2f29e35013f8489da
navyasonal <navyasonal07@gmail.com>
2022-09-04 20:45:19 +0530

    New code

---
+++ linkedlistinsertion.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+void linkedlistprint(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct Node * insertatfirst(struct Node *head,int data)
+{
+    struct Node *p =(struct Node *)malloc (sizeof(struct Node));
+    p->next =head;
+    p->data=data;
+    return p;
+}
+
+int main()
+{
+    // allocate memory for nodes in the heap
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+
+    // linking first and second node
+    head->data = 7;
+    head->next = second;
+
+    second->data = 8;
+    second->next = third;
+
+    third->data = 10;
+    third->next = NULL;
+    linkedlistprint(head);
+    head=insertatfirst(head,56);
+    linkedlistprint(head);
+    return 0;
+}
