c0164604aed3c9c781a0b2cdc05ab91ce2a6b23c
navyasonal <navyasonal07@gmail.com>
2022-09-07 22:48:21 +0530

    Adding files

--- Theory_Assignment/21052509_DSA_Assgn2.c
+++ Theory_Assignment/21052509_DSA_Assgn2.c
@@ -14,7 +14,7 @@ void create(int n)
     int num;
     stnode  = (struct node*)malloc(sizeof(struct node));  // head node
 
-    num = rand() % 100 + 1;
+    num = rand() %101;
 
     stnode->num=num;
     stnode->nextptr=NULL;
@@ -32,7 +32,7 @@ void create(int n)
     
     else
     {
-    num = rand() % 100 + 1;
+    num = rand() %101;
 
     fnode->num=num;
     fnode->nextptr=NULL;
@@ -93,6 +93,47 @@ void display()
         }
     }
 }
+
+void middlenode(struct node *head)
+{
+    struct node *p, *q;
+    int flag = 0;
+ 
+    q = p = head;
+    /*for every two hops of q, one hop for p*/
+    while (q->nextptr != NULL)
+    {
+        q = q->nextptr;
+        if (flag)
+        {
+            p = p->nextptr;
+        }
+        flag = !flag;
+    }
+    if (flag)
+    {
+        printf("List contains even number of nodes\nThe middle two node's values are: %d \n", p->nextptr->num);
+    }
+    else
+    {
+        printf("The middle node of the list is: %d\n", p->num);
+    }
+}
+void search(struct node *head)
+{
+   
+    int data=rand() %101;
+    
+    struct node* current = head; 
+    while (current != NULL)
+    {
+        if (current->num == data)
+            printf("The random generated number %d is present in the randomly genrated list ",data);
+        current = current->nextptr;
+    }
+    printf("The random generated number %d is not present in the randomly genrated list ",data);
+}
+
 int main()
 {
     int n,data;
@@ -104,6 +145,9 @@ int main()
     bubbleSort(stnode);
     printf("The values in nodes are \n");
     display();
+    middlenode(stnode);
+    search(stnode);
+    
     return 0;
 }
 
